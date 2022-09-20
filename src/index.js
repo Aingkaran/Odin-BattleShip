e687e98f19c5bd27464b9d16301484672bcae112
Aingkaran <aingkaran1995@gmail.com>
2022-09-19 20:36:14 -0400

    Create Ship object

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 import './style.css';
+import { ship} from './ships'
+
+
+const newship= ship(4);
+
+console.log(newship.length)
+console.log(newship.shipContainer)
+
+
+newship.hit(0)
+newship.hit(1)
+newship.hit(3)
+
+console.log(newship.shipContainer)
+console.log(newship.isSunk())
+
+newship.hit(2)
+console.log(newship.isSunk())
---
+++ src/ships.js
@@ -0,0 +1,40 @@
+const ship =(size)=>{
+
+    let container= ()=>{
+        let shipsize= []
+        let addSize=""
+        for (let i=0;i<size;i++){
+            shipsize.push(addSize)
+        }
+        return shipsize
+    }
+    let shipContainer = container()
+    let length = container().length
+
+    const hit=(position)=>{
+        shipContainer[position]="hit"
+    }
+    
+
+    const isSunk=()=>{
+        let count=0
+        for (let i=0;i<length;i++){
+            if (shipContainer[i]=="hit"){
+                count=count+1
+            }
+        }
+        if (count==length){
+            return true 
+            console.log(true)
+        }
+        else {
+            return false
+            console.log(false)
+        }
+    }
+
+return {container, length, hit, isSunk, shipContainer}
+
+}
+
+export {ship}
