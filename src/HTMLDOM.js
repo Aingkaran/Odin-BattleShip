3636e69699144dde2e963cb9750ea7db1779fc5c
Aingkaran <aingkaran1995@gmail.com>
2022-09-25 21:39:43 -0400

    Add ships to the DOM

--- src/HTMLDOM.js
+++ src/HTMLDOM.js
@@ -3,6 +3,37 @@ import {Gameboard} from './Gameboard'
 import {Player} from './Player'
 
 
-const createGrid=(DOMelement))=>{
-    const grid= document.querySelector(DOMelement)
+const createGrid=(DOMelement, Playerboard)=>{
+    const grid= document.querySelector(String(DOMelement))
+
+    for(let i=0;i<100;i++){
+        const oneGrid= document.createElement('div')
+        oneGrid.setAttribute("class","boxes")
+        oneGrid.setAttribute("id",String(Playerboard.PlayerName) +String(i))
+
+
+        grid.appendChild(oneGrid)
+    }
+
+  
+
 }
+
+const placeDOMShips=(Player)=>{
+
+    for (let i=0; i<Player.PlayerBoard.box.length;i++){
+
+        if (Player.PlayerBoard.box[i]!=""){
+            const shipBox = document.getElementById(String(Player.PlayerName) + String(i))
+            shipBox.setAttribute("id","ship")
+        }
+
+    }
+
+}
+
+
+
+
+
+export {createGrid, placeDOMShips}
--- src/index.js
+++ src/index.js
@@ -1,24 +1,35 @@
 import './style.css';
 import { ship} from './ships'
 import {Gameboard} from './Gameboard'
+import {Player} from './Player'
+import {createGrid, placeDOMShips} from './HTMLDOM'
 
 
 
 
-const longShip= ship(4,"L")
-const mediumShip=ship(3,"M")
-const smallShip1= ship(2,"S1")
+const PlayerOne = Player("Aingkaran")
+
+const Computer = Player("AI")
+
+createGrid(".PlayerBoard", PlayerOne)
+createGrid(".ComputerBoard", Computer)
+
+ 
 const smallShip2= ship(2,"S2")
+const mediumShip=ship(3,"M")
+const mediumShip2=ship(3,"M2")
+const largeShip =ship(4,"L")
 
-const playerGameboard= Gameboard()
+PlayerOne.PlayerBoard.placeShip(smallShip2)
+PlayerOne.PlayerBoard.placeShip(mediumShip)
+PlayerOne.PlayerBoard.placeShip(mediumShip2)
+PlayerOne.PlayerBoard.placeShip(largeShip)
 
-console.log(playerGameboard.box)
 
-playerGameboard.placeShip(longShip)
-playerGameboard.placeShip(mediumShip)
-playerGameboard.placeShip(smallShip1)
-playerGameboard.placeShip(smallShip2)
+Computer.PlayerBoard.placeShip(smallShip2)
+Computer.PlayerBoard.placeShip(mediumShip)
+Computer.PlayerBoard.placeShip(mediumShip2)
+Computer.PlayerBoard.placeShip(largeShip)
 
-console.log(playerGameboard.box)
-
-console.log(playerGameboard.shipArray[0].shipContainer)
+placeDOMShips(PlayerOne)
+placeDOMShips(Computer)
